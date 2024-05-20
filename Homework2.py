f02f5d783d4c3701f940ebf6b34e7256e777a987
AMalakhov <spamarkady@yandex.ru>
2024-05-20 20:30:00 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,6 @@
+a = total_tasks = 12
+b = time_spent = 1.5
+c = name_of_cource = 'Python'
+d = comma = ','
+colon = e = ':'
+print('Курс:',c,d,'всего задач:',a,d,'затрачено часов:',b,d,'среднее время выполнения:',b/a)
